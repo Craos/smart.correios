afb521d7bf05348e72ddf8e82e2eeda34bb2feee
Oberdan Brito <oberdan@craos.net>
2019-06-06 12:17:47 -0300

    Versão 5 operacional

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <title>Smart Auth</title>
+
+    <meta name="description" content="Verticals">
+    <meta name="author" content="Oberdan Brito">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <script type="text/javascript" src="../smt/lib/dhtmlx/codebase/dhtmlx.js?<?php echo time(); ?>"></script>
+    <script type="text/javascript" src="../smt/lib/moment/moment.js?<?php echo time(); ?>"></script>
+    <script type="text/javascript" src="../smt/lib/moment/moment-with-locales.js?<?php echo time(); ?>"></script>
+    <script type="text/javascript" src="../smt/lib/jquery/jquery-3.4.1.js"></script>
+    <script type="text/javascript" src="js/operacoes.js?<?php echo time(); ?>"></script>
+    <script type="text/javascript" src="js/screencast.js?<?php echo time(); ?>"></script>
+    <script type="text/javascript" src="js/notificacao.js?<?php echo time(); ?>"></script>
+    <script type="text/javascript" src="js/autorizacao.js?<?php echo time(); ?>"></script>
+    <script type="text/javascript" src="js/unidade.js?<?php echo time(); ?>"></script>
+    <script type="text/javascript" src="js/historico.js?<?php echo time(); ?>"></script>
+    <script type="text/javascript" src="js/entrada.js?<?php echo time(); ?>"></script>
+    <script type="text/javascript" src="js/modelos.js?<?php echo time(); ?>"></script>
+    <script type="text/javascript" src="js/webservice.js?<?php echo time(); ?>"></script>
+    <script type="text/javascript" src="js/pesquisa.js?<?php echo time(); ?>"></script>
+    <script type="text/javascript" src="js/main.js?<?php echo time(); ?>"></script>
+    <link type="text/css" rel="stylesheet" href="../smt/lib/dhtmlx/skins/material/dhtmlx.css">
+    <link rel="stylesheet" type="text/css" href="../smt/lib/dhtmlx/codebase/fonts/font_roboto/roboto.css"/>
+    <link rel="stylesheet" type="text/css" href="../smt/lib/dhtmlx/codebase/fonts/font_awesome/css/font-awesome.min.css"/>
+    <link type="text/css" rel="stylesheet" href="css/index.css?<?php echo time(); ?>">
+</head>
+<body>
+</body>
+</html>
